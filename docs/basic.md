7df99e124060e91360f0f842543134ac10b5056d
zhangming <qianzm2490@vip.qq.com>
2020-04-26 22:33:02 +0800

    feat: create the project

---
+++ docs/basic.md
@@ -0,0 +1,15 @@
+基本用法
+
+```vue
+<template>
+  <iview-tree-transfer />
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+```
---
+++ src/iview-tree-transfer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="iview-tree-transfer" v-bind="$attrs" v-on="$listeners">
+    hello
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IviewTreeTransfer",
+  props: {},
+  data() {
+    return {};
+  }
+};
+</script>
